e65295247ba00d0e2c15d0067fed7b2519a4ac80
victordelval <victor.val.andres@gmail.com>
2019-03-16 11:17:45 +0100

    Add auth HOC from amplify-react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 
 class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-l">Amplify Notetaker</h1>
+        <form className="mb3">
+          <input 
+            type="text" 
+            className="pa2 f4"
+            placeholder="Write your note" />
+          <button className="pa2 f4" type="submit">
+            Add Note
+          </button>
+        </form>
+        
+      </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
